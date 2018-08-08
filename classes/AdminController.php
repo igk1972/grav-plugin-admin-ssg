5eaa07c8ddbfe32a204e187663869dd243a3bb47
Igor Kuznetsov <igk@igk.ru>
2018-08-08 23:57:33 +0300

    Controllers.

---
+++ classes/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Grav\Plugin\Publish;
+
+use Grav\Common\Grav;
+use Grav\Common\Helpers\Base32;
+use Grav\Common\Utils;
+use Grav\Plugin\Publish\Controller;
+
+/**
+ * Class Controller
+ * @package Grav\Plugin\Directory
+ */
+class AdminController extends Controller
+{
+
+
+}
